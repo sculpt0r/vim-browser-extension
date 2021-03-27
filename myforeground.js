ec0fdd6498a4016a17c1e8031737765a8a65f2bc
sculpt0r <b.kalemba@cksource.com>
2021-03-27 15:59:57 +0100

    Kind of working moving cursor

--- myforeground.js
+++ myforeground.js
@@ -1,5 +1,6 @@
 console.log('Im in front!');
-let isPluginActive = false;
+let isPluginActive = false,
+    mode = 'move'; //'move' || 'input'
 
 function KeyPress(e) {
     if ( e.key === 'v' && e.altKey ) {
@@ -10,6 +11,42 @@ function KeyPress(e) {
 function togglePluginActive(){
     isPluginActive = !isPluginActive;
     console.log('Plugin is now', isPluginActive);
+
+    if( isPluginActive ) {
+        activateVim();
+    } else {
+        deactivateVim();
+    }
+}
+
+function activateVim() {
+    mode = 'move';
+
+    document.onkeydown  = handleMoveKeys;
+}
+
+function handleMoveKeys( e ) {
+    if( ['h','j','k','l'].includes( e.key ) ) {
+        console.log('it was nav key!!');
+        switch ( e.key ) {
+            case 'h':
+                MoveCarret( -1 );
+            break;
+            case 'l':
+                MoveCarret( 1 );
+            break;
+        }
+    }
+    e.preventDefault();
+}
+
+function MoveCarret( direction ) {
+    document.activeElement.selectionStart += direction; 
+    document.activeElement.selectionEnd += direction; 
+}
+
+function deactivateVim() {
+
 }
 
 document.onkeydown = KeyPress;
