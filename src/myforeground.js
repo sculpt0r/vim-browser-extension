2ae435a364890bfb4587e783d0384e24bbd0cd18
sculpt0r <b.kalemba@cksource.com>
2021-04-07 00:32:30 +0200

    rewrite on classes - key bindings are failing

--- src/myforeground.js
+++ src/myforeground.js
@@ -1,89 +1,185 @@
-console.log('VIM typer!');
-let mode;
-// let isPluginActive = false,
-//     mode = 'move'; //'move' || 'input';
+// console.log('VIM typer!');
 
-document.onkeydown = HandlePluginToggle;
+class Mode {
+    deactivate() {
+
+    }
+    activate() {
+
+    }
+}
+class ModeManager {
+    constructor() {
+        this.currentMode = new EmptyMode();
+    }
+
+    changeMode(newMode) {
+        if( this.currentMode.constructor === newMode.constructor ) {
+            return;
+        }
+
+        if(this.currentMode) {
+            this.currentMode.deactivate();
+        }
+
+        this.currentMode = newMode;
+        this.currentMode.activate();
+    }
+
+    anyMode() {
+        return !( this.currentMode instanceof EmptyMode );
+    }
+
+}
+class InsertMode extends Mode {
+    constructor() {
+        super();
+        document.activeElement.selectionEnd = document.activeElement.selectionStart;
+        console.log('constructor insertu');
+    }
+
+    activate() {
+        super.activate();
+        console.log('activate insertu');
+    }
+    deactivate() {
+        super.deactivate();
+        console.log('deactiv inseru');
+    }
+
+}
+
+class NavigationMode extends Mode {
+    constructor() {
+        super();
+    
+
+        console.log('navmode constructor');
+    }
+
+    activate() {
+        super.activate();
+        console.log('activate nav mode');
+
+        document.addEventListener( 'keydown', this.handleKeys);
+
+        document.activeElement.selectionEnd = document.activeElement.selectionStart;
+        document.activeElement.selectionEnd++;
+    }
+
+    deactivate() {
+        super.deactivate();
+        console.log('deactivate nav mdoe');
+
+        document.removeEventListener( 'keydown', this.handleKeys );
+    }
+
+    handleKeys( e ) {
+        if( ['h','j','k','l'].includes( e.key ) ) {
+    
+            console.log('it was nav key!!', this);
+    
+            switch ( e.key ) {
+                case 'h':
+                    this.MoveCarret( -1 );
+                break;
+                case 'l':
+                    this.MoveCarret( 1 );
+                break;
+                case 'j':
+                    this.CalculateHorizontal( 1 );
+                break;
+                case 'k':
+                    this.CalculateHorizontal( -1 );
+                break;
+            }
+        }
+    
+        e.preventDefault();
+    }
+
+    CalculateHorizontal( direction ) {
+        const start = document.activeElement.selectionStart,
+            content = getContent( document.activeElement ),
+            nextLineBreakIndex = content.indexOf('\n', start),
+            prevLineBreakIndex = content.lastIndexOf('\n', start),
+            distanceFromLineBegin = start - prevLineBreakIndex;
+    
+        let newPos = start;
+    
+        if(direction > 0) {
+            newPos = nextLineBreakIndex + distanceFromLineBegin;
+        } else {
+            const doublePrevLineBreakIndex = content.lastIndexOf('\n' , prevLineBreakIndex-1);
+            newPos = doublePrevLineBreakIndex + distanceFromLineBegin;
+        }
+    
+        document.activeElement.selectionEnd = document.activeElement.selectionStart = newPos;
+        document.activeElement.selectionEnd++;
+    }
+
+    MoveCarret( direction ) {
+        //     let p = document.activeElement; 
+        // let range = new Range();
+        //   range.setStart(p.firstChild, 0);
+        //   range.setEnd(p.firstChild, 2);
+        // document.getSelection().removeAllRanges();
+        // document.getSelection().addRange(range);
+
+        var start = document.activeElement.selectionStart;
+
+        var newPos = clamp( start + direction, 0, getContent( document.activeElement ).length -1 );
+
+        document.activeElement.selectionEnd = document.activeElement.selectionStart = newPos;
+        document.activeElement.selectionEnd++;
+    }
+    
+}
+
+class EmptyMode extends Mode {
+    deactivate() {
+        super.deactivate();
+        console.log('deactiv empty');
+    }
+
+    activate() {
+        super.activate();
+        console.log('active empty');
+    }
+}
+const modeMgr  = new ModeManager();
+
+document.addEventListener( 'keydown', HandlePluginToggle );
 
 function HandlePluginToggle(e) {
 	if( e.key === 'v' && e.altKey ) {
-		if( !mode ) {
-			activateInsertMode();
-			setIndicator( true );
-		} else {
-			mode = null;
+		if( modeMgr.anyMode() ) {
+            modeMgr.changeMode(new EmptyMode());
 			setIndicator( false );
 			//unpin all listeners etc....
+        } else {
+            modeMgr.changeMode(new InsertMode());
+			setIndicator( true );
 		}
 		return;
 	}
 
-	if( mode ) {
-		if( e.key === 'Escape' ) {
-			activateMoveMode();
+	if( modeMgr.anyMode() ) {
+		if( e.key === 'Escape' && ! (modeMgr.currentMode instanceof NavigationMode ) ) {
+            modeMgr.changeMode(new NavigationMode());
 		}
 
-		if ( mode === 'move' && ( e.key === 'i' || e.key === 'a' ) ) {
-			activateInsertMode();
+		if ( modeMgr.currentMode instanceof NavigationMode && ( e.key === 'i' || e.key === 'a' ) ) {
+            modeMgr.changeMode(new InsertMode());
 			e.preventDefault();
 		}
 	}
 }
 
-function activateMoveMode() {
-    mode = 'move';
-    document.activeElement.selectionEnd = document.activeElement.selectionStart;
-    document.activeElement.selectionEnd++;
-    document.onkeydown  = handleMoveKeys;
-}
 
-function handleMoveKeys( e ) {
-    HandlePluginToggle(e);
-    
-    if( mode !== 'move' ){ return; }
 
-    if( ['h','j','k','l'].includes( e.key ) ) {
 
-        console.log('it was nav key!!');
 
-        switch ( e.key ) {
-            case 'h':
-                MoveCarret( -1 );
-            break;
-            case 'l':
-                MoveCarret( 1 );
-            break;
-            case 'j':
-                CalculateHorizontal( 1 );
-            break;
-            case 'k':
-                CalculateHorizontal( -1 );
-            break;
-        }
-    }
-
-    e.preventDefault();
-}
-
-function CalculateHorizontal( direction ) {
-    const start = document.activeElement.selectionStart,
-        content = getContent( document.activeElement ),
-        nextLineBreakIndex = content.indexOf('\n', start),
-        prevLineBreakIndex = content.lastIndexOf('\n', start),
-        distanceFromLineBegin = start - prevLineBreakIndex;
-
-    let newPos = start;
-
-    if(direction > 0) {
-        newPos = nextLineBreakIndex + distanceFromLineBegin;
-    } else {
-        const doublePrevLineBreakIndex = content.lastIndexOf('\n' , prevLineBreakIndex-1);
-        newPos = doublePrevLineBreakIndex + distanceFromLineBegin;
-    }
-
-    document.activeElement.selectionEnd = document.activeElement.selectionStart = newPos;
-    document.activeElement.selectionEnd++;
-}
 
 function getContent( element ) {
     if( element.tagName === 'TEXTAREA' ) {
@@ -93,26 +189,7 @@ function getContent( element ) {
     }
 }
 
-function MoveCarret( direction ) {
-//     let p = document.activeElement; 
-// let range = new Range();
-//   range.setStart(p.firstChild, 0);
-//   range.setEnd(p.firstChild, 2);
-// document.getSelection().removeAllRanges();
-// document.getSelection().addRange(range);
 
-    var start = document.activeElement.selectionStart;
-
-	var newPos = clamp( start + direction, 0, getContent( document.activeElement ).length -1 );
-
-    document.activeElement.selectionEnd = document.activeElement.selectionStart = newPos;
-    document.activeElement.selectionEnd++;
-}
-
-function activateInsertMode() {
-    mode = 'insert';
-    document.activeElement.selectionEnd = document.activeElement.selectionStart;
-}
 
 function clamp(value, min, max) {
 	return Math.min(Math.max(value, min), max);
@@ -135,4 +212,3 @@ function setIndicator( isActive ) {
 window.addEventListener('DOMContentLoaded', function(event) {
 	createIndicator();
 });
-
