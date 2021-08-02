eb436559cff0a8ffb38df538f37fa145a5457d43
sculpt0r <b.kalemba@cksource.com>
2021-08-02 09:20:04 +0200

    Dump latest changes

---
+++ src/selection.js
@@ -0,0 +1,28 @@
+function setSelection( start, end, element ) {
+
+	if( element.tagName === 'TEXTAREA' ) {
+		element.selectionStart = start;
+		element.selectionEnd = end;
+	} else {
+		return;
+		let range = new Range();
+
+		range.setStart( element.firstChild, start );
+		range.setEnd( element.firstChild, end );
+
+		document.getSelection().removeAllRanges();
+		document.getSelection().addRange( range );
+	}
+}
+
+function getCarretStart( element ) {
+	if( element.tagName === 'TEXTAREA' ) {
+		return element.selectionStart;
+	} else {
+		return null;
+		const selObj = window.getSelection();
+		const selRange = selObj.getRangeAt(0);
+
+		return selRange.startOffset;
+	}
+}
