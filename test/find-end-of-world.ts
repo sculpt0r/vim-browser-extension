09cbeefc015f7a72fd3f342e13af4df4ed762a6f
sculpt0r <contact@sculpt0r.net>
2021-10-30 10:23:26 +0200

    fully supported ts

--- test/find-end-of-world.ts
+++ test/find-end-of-world.ts
@@ -1,15 +1,14 @@
-/* eslint-disable no-undef */
-const assert = require( 'assert' );
-const { FindEndOfWord } = require( '../src/content-helper' );
+import test from 'ava';
+import { FindEndOfWord } from '../src/content-helper';
 
-describe( 'Content operations', () => {
-	it( 'find the nearest end of word', () => {
-		const index = FindEndOfWord( 0, 'This is it' );
-		assert.equal( index, 3, 'Invalid end of word index' );
-	} );
-
-	it( 'find the nearest end of word 2', () => {
-		const index = FindEndOfWord( 5, 'This is it' );
-		assert.equal( index, 6, 'Invalid end of word index' );
-	} );
+// describe( 'Content operations', () => {
+test( 'find the nearest end of word', t => {
+	const index = FindEndOfWord( 0, 'This is it' );
+	t.deepEqual( index, 3, 'Invalid end of word index' );
 } );
+
+test( 'find the nearest end of word 2', t => {
+	const index = FindEndOfWord( 5, 'This is it' );
+	t.deepEqual( index, 6, 'Invalid end of word index' );
+} );
+// } );
