4df23c2c0ad441c70cbfbb29f2e1bfe55e75221e
sculpt0r <contact@sculpt0r.net>
2022-08-12 06:49:16 +0200

    Improved end word finding

--- src/content-helper.ts
+++ src/content-helper.ts
@@ -1,20 +1,36 @@
 
 
 function FindEndOfWord( currentPosition : number, content : string ) : number {
+	//find last non empty char followed by empty char
+	const lastNonEmptyChar = /[^(\s\n)][\s]{1}/g;
+
+	lastNonEmptyChar.lastIndex = currentPosition + 1;
+
+	const res = lastNonEmptyChar.exec( content );
+	if ( res === null ) {
+		// if nth found - could mean there is no empty char at the end
+		// try to find last non empty char at the end of string
+		const lastNonEmptyChar = /[^(\s\n)]$/g;
+		const res2 = lastNonEmptyChar.exec( content );
+
+		if ( res2 === null ) {
+			return currentPosition;
+		}
+
+		return res2.index;
+	}
+
+	return res.index;
+}
+
+function FindBeginOfNextWord( currentPosition : number, content : string ) : number {
 	const wordBreakRegEx = /[^\s]{1}(\s|$|\n)/g;
 	wordBreakRegEx.lastIndex = currentPosition;
 
 	wordBreakRegEx.test( content );
-	if ( wordBreakRegEx.lastIndex-2 === currentPosition ) {
-		//end of line dont bump index so much
-		return FindEndOfWord( currentPosition +1, content ) + 1;
-	}
-	// if ( wordBreakRegEx.lastIndex - 2 === currentPosition ) {
 
-	// 	return FindEndOfWord( currentPosition+2, content );
-	// }
-
-	return wordBreakRegEx.lastIndex - 2;
+	const closesEnd = wordBreakRegEx.lastIndex > content.length ?
+		currentPosition : wordBreakRegEx.lastIndex;
+	return closesEnd;
 }
-
 export { FindEndOfWord };
